05a5672ffa96a6791093002d130ebc431a676964
Alexandru Ionascu <alexandruionascu.bd@gmail.com>
2017-05-19 10:10:05 +0300

    succesfully scrapped match results

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -27,12 +28,48 @@ public class Main {
     public static void main(String[] args) throws Exception {
         WebDriver driver = new ChromeDriver();
         driver.get("https://stats.betradar.com/s4/?clientid=1391#2_1,3_1,22_1,5_32887,9_teampage,6_38");
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("odd")));
-        List<WebElement> elements = driver.findElements(By.cssSelector("tr"));
-        for(WebElement e : elements) {
-            System.out.println(e.getText());
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("couch_lastx")));
+        WebElement downArrow = driver.findElement(By.cssSelector(".couch_lastx tfoot"));
+        int previousHash = 0;
+        boolean scrolledBottom = false;
+        while(!scrolledBottom) {
+            int currentHash = 0;
+            //get the elements
+            List<WebElement> elements = driver.findElements(By.cssSelector(".couch_lastx tbody tr"));
+            for(WebElement tableRow : elements) {
+                String text = tableRow.getText().trim();
+                if(text.length() > 0) {
+                    String[] matchData = text.split(" ");
+                    String date = matchData[0];
+                    String homeTeam = matchData[1];
+                    String awayTeam = matchData[3];
+                    String score = matchData[4];
+                    String fixtureNo = matchData[5];
+
+                    
+                    System.out.println(text);
+                    System.out.println("--------------");
+                    currentHash += text.hashCode();
+                }
+            }
+
+            for(int j = 1; j <= 5; j++) {
+                downArrow.click();
+            }
+
+
+
+
+            if(previousHash == currentHash)
+                scrolledBottom = true;
+
+            previousHash = currentHash;
+
         }
+
+
+        System.out.println("dsadas");
 
     }
 }
